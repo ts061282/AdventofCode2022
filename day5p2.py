56f06555a8c484e03b1928aa92534f9cbdbc1351
ts061282 <tyler.j.shogren@gmail.com>
2022-12-04 23:55:05 -0600

    Day 5 solution files, and solution template

---
+++ day5p2.py
@@ -0,0 +1,28 @@
+input = open("input.txt")
+
+solution = ""
+
+stack1 = 'NBDTVGZJ'
+stack2 = 'SRMDWPF'
+stack3 = 'VCRSZ'
+stack4 = 'RTJZPHG'
+stack5 = 'TCJNDZQF'
+stack6 = 'NVPWGSFM'
+stack7 = 'GCVBPQ'
+stack8 = 'ZBPN'
+stack9 = 'WPJ'
+stacks = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
+
+for line in input:
+    # solve
+    moves = int(line.split(" from ")[0].split("move ")[1])
+    source = int(line.split(" from ")[1].split(" to ")[0])
+    destination = int(line.split(" to ")[1].strip())
+    stacks[destination-1] += stacks[source-1][-moves:]
+    stacks[source-1] = stacks[source-1][:-moves]
+
+        
+for stack in stacks:
+    solution += stack[-1]
+
+print(solution)
---
+++ dayxp1.py
@@ -0,0 +1,9 @@
+input = open("OneDrive\\Documents\\input.txt")
+
+solution = 0
+
+for line in input:
+    # solve
+    print("test")
+
+print(solution)
